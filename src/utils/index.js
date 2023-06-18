49f16290ca69f8a09a354fd33197a5ded01e1c91
rezasetyawan <setyawanreza960@gmail.com>
2023-06-18 13:20:08 +0700

    add albums and songs plugin

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+const mapSongsDBToModel = ({id, title, year, performer, genre, duration, album_id}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = {mapSongsDBToModel};
+
